2b21f9e6fa542d675036d4b5c9d8b8d48ad770b2
snezhanadosinova <sdosinova@gmail.com>
2023-01-30 13:59:55 +0200

    feat: used useMemo to filter the numbers only and change the icon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
+import { useState, useMemo } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [isNumber, setIsNumber] = useState(false);
+
+  const handleChange = (text, isNumber) => {
+    const onlyNumbers = text.replace(/\D/g, "");
+    setText(text);
+    if (text === onlyNumbers) {
+      // setText(text);
+      setIsNumber(true);
+    } else {
+      // setText(text);
+      setIsNumber(false);
+    }
+    return isNumber;
+  };
+
+  const memoizedValue = useMemo(
+    () => handleChange(text, isNumber),
+    [text, isNumber]
+  );
+
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className={"fas " + (memoizedValue ? "fa-check" : "fa-times")} />
+        </span>
       </div>
     </div>
   );
